5d11a1787c7ae4b3ed43871216027f47226f8cac
metacoder87 <gary.miller@gmx.com>
2020-09-26 16:16:12 -0400

    Tries to search the greater than before less than

--- recursion.rb
+++ recursion.rb
@@ -233,18 +233,18 @@ puts "Binary Search"
         #checks if target is the middle element
         if target == array[array.count / 2] 
             brr.index(array[array.count / 2]) # returns middle element
-        # checks if target is smaller than the smallest element
-        elsif target < array[0] 
-            [].first # returns nil
-        # checks if target is smaller than the middle element
-        elsif target < array[array.count / 2] 
-            bsearch(array.take(array.count / 2), target) # runs bsearch on the lesser than sub array
         # checks if target is greater than the greatest element
         elsif target > array.last 
             [].first # returns nil
         # checks if target is greater than the middle element
         elsif target > array[array.count / 2] 
             bsearch(array.values_at(array.count / 2 + 1..-1), target) # runs bsearch on the greater than sub array
+        # checks if target is smaller than the smallest element
+        elsif target < array[0] 
+            [].first # returns nil
+        # checks if target is smaller than the middle element
+        elsif target < array[array.count / 2] 
+            bsearch(array.take(array.count / 2), target) # runs bsearch on the lesser than sub array
         end
     end
 
